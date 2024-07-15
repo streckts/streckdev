fcc60fed12e5a5bcae9da79171ac78847bd43b32
Streck Salmon <streck0101@gmail.com>
2024-07-15 15:37:02 -0400

    Fixed margin for accordion, added OR project

--- static/main.js
+++ static/main.js
@@ -8,21 +8,9 @@ document.addEventListener('DOMContentLoaded', (event) => {
             content.style.display = content.style.display === 'block' ? 'none' : 'block';
         });
     });
-});
 
-const toolColors = {
-    'HTML': 'blue',
-    'CSS': 'green',
-    'JavaScript': 'gold',
-    'Python': 'yellowgreen',
-    'React': 'skyblue',
-    'Django': 'darkgreen'
-    // Add more tools and their corresponding colors here
-};
-
-document.querySelectorAll('.tool').forEach(tool => {
-    const toolName = tool.textContent.trim();
-    if (toolColors[toolName]) {
-        tool.style.backgroundColor = toolColors[toolName];
-    }
+    document.querySelectorAll('.tool').forEach(tool => {
+        const toolName = tool.textContent.trim().toLowerCase();
+        tool.classList.add(toolName);
+    });
 });
