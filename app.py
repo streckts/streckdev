4f6245d6d2e43004f1a3b46ef3f24c4ea6a229a0
Streck Salmon <streck0101@gmail.com>
2024-07-16 12:06:25 -0400

    Working on login, initialized user class, removed venv from github

--- app.py
+++ app.py
@@ -1,19 +1,52 @@
-from flask import Flask, render_template, redirect, url_for, session, jsonify, request
+from flask import Flask, render_template, redirect, url_for, session, jsonify, request, flash
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from flask_sqlalchemy import SQLAlchemy
-
+from werkzeug.security import generate_password_hash, check_password_hash
 import os
+
+from models import db, User
+
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.db')
-#app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
 
-#db = SQLAlchemy(app)
+db.init_app(app) # initialize db
 
 @app.route("/")
 def home():
     #if 'logged_in' not in session:
     #    return redirect(url_for('login'))  # Assumes you have a 'login' route defined
     return render_template('home.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        user = User.query.filter_by(email=email).first()
+        if user and check_password_hash(user.password, password):
+            login_user(user)
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check email and password', 'danger')
+    return render_template('login.html')
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+        hashed_password = generate_password_hash(password, method='sha256')
+        new_user = User(username=username, email=email, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        flash('Your account has been created! You can now log in', 'success')
+        return redirect(url_for('login'))
+    return render_template('signup.html')
 
 @app.route("/azure")
 def azure():
@@ -26,8 +59,8 @@ def portfoliotracker():
 if __name__ == "__main__":
 
     # Initialize database w/ SQLite
-    #with app.app_context():
-    #    db.create_all()
+    with app.app_context():
+        db.create_all()
 
     # Run site
     app.run(debug=True)
---
+++ portfolio_tracker.py
@@ -0,0 +1,8 @@
+import pandas as pandas
+import altair as alt
+
+def retrieveUserData():
+    return None
+
+def displayChart():
+    return None
