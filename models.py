16ea9c1f2a0d7cf444a3598b880675ec11def9c4
Streck Salmon <streck0101@gmail.com>
2024-07-19 18:17:27 -0400

    Updated database to account for assets

--- models.py
+++ models.py
@@ -8,3 +8,14 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(150), unique=True, nullable=False)
     email = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
+    assets = db.relationship('Asset', backref='owner', lazy=True)
+
+class Asset(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(150), nullable=False)
+    ticker = db.Column(db.String(10), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    type = db.Column(db.String(50), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+
