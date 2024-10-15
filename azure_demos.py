153014c1a6e1c193dfcfd263f3b01e41cf05147c
StrecK Salmon <streck0101@gmail.com>
2024-10-15 15:29:22 -0400

    Implemented translation function

--- azure_demos.py
+++ azure_demos.py
@@ -8,6 +8,7 @@ import logging
 from models import db, User, Asset, UserAsset
 
 azure_demos_bp = Blueprint('azure_demos', __name__)
+load_dotenv()
 
 @azure_demos_bp.route('/azure_demos')
 @login_required
@@ -16,3 +17,31 @@ def azure_demos():
         return render_template('azure_demos.html')
     else:
         return redirect(url_for('/login'))
+
+@azure_demos_bp.route('/translate', methods=['POST'])
+@login_required
+def translate():
+    input = request.form['input']
+    language = request.form['language']
+    text = translation_engine(input, language)
+    return render_template('azure_demos.html', text=text[0].translations[0].text)
+
+# Azure functions
+from azure.core.credentials import AzureKeyCredential
+from azure.ai.translation.text import TextTranslationClient
+
+def translation_engine(input, language):
+    credential = AzureKeyCredential(os.getenv('TRANSLATOR_API_KEY'))
+    endpoint = os.getenv('TRANSLATOR_ENDPOINT')
+
+    translation_client = TextTranslationClient(endpoint=endpoint, credential=credential)
+    text = [input]
+    language = [language]
+    translated_text = translation_client.translate(text, to_language=language)
+    return translated_text
+
+def ocr_engine():
+    return 0
+
+def speech_engine():
+    return 0
